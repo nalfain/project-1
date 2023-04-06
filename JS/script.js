847e86b43148d1706d3729c7eeaf7afe13acf11f
Piratorov <piratorov@gmail.com>
2023-04-06 17:44:50 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,61 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm('Вам есть 18?');
+
+//console.log(result);
+
+//const answer = +prompt('Сколько Вам лет?', '18');
+//console.log(typeof(answer + 5));
+//console.log(answer + 5);
+
+//const answers = [];
+//answers[0] = prompt('Name', '');
+//answers[1] = prompt('Sername', '');
+//answers[2] = prompt('age', '');
+
+//document.write(answers);
+//console.log(answers);
+//console.log(typeof(answers));
+
+
+
+
+//const category = 'toys';
+
+//console.log(`https://someeurl.com/${category}/5`);
+
+//const user = `ivan`;
+//alert(`Привет мой дорогой, ${user}`)
+
+console.log(`arr`+ `-object`);
+console.log(4+`5`);
+//
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+//
+console.log(5%2);
+console.log(2*4 == 8);
+console.log(2*4 == `8`);
+console.log(2*4 === `8`);
+console.log(2 + 2 * 2 !== `8`);
+console.log(2 + 2 * 2 == `6`);
+console.log(2 + 2 * 2 !== `6`);
+console.log(2 + 2 * 2 !== 6);
+console.log(2 + 2 * 2 !== `6`);
+//
+const isChacked = true,
+    isClose = false;
+    
+console.log(isChacked && isClose);
+console.log(isChacked || isClose);
+console.log(isChacked && !isClose);
+
+//порядок выполнения операций (см табличку).
